109d33090a0bbe88fdf6f703407bdd54e0a9516c
hocknang <fang_peiyuan@yahoo.com>
2024-10-11 14:30:09 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -4,4 +4,12 @@ import streamlit as st
 def home():
     st.title("Term & Condition")
     st.write('Hi this is Term & Condition Explorer ChatBot')
+    
+    # Dropdown with options
+    options = ["Search Using Provided Url", "Option 2"]
 
+    # Create a dropdown selectbox
+    selected_option = st.selectbox("Choose an option:", options)
+
+    # Display the selected option
+    st.write(f"You selected: {selected_option}")
