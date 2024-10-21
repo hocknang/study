5f3fd4e2e1d62911eda141c2721f5c10362f4990
hocknang <fang_peiyuan@yahoo.com>
2024-10-21 16:38:23 +0800

    new file

--- lesson.py
+++ lesson.py
@@ -5,49 +5,16 @@ from io import BytesIO
 from openai import OpenAI
 
 
-def chatbot():
-    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-                model=st.session_state["openai_model"],
-                messages=[
-                    {"role": m["role"], "content": m["content"]}
-                    for m in st.session_state.messages
-                ],
-                stream=True,
-            )
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
-
 def pdfReader(document_url, uploaded_file):
     if document_url is not None:
         st.write(f"URL provided: {document_url}")
         response = requests.get(document_url)
         readPDF(response)
-        chatbot()
         #
     elif uploaded_file is not None:
         # Working code to read the pdf content
         pdf_reader = PdfReader(uploaded_file)
         readContentPDF(pdf_reader)
-        chatbot()
         #
     else:
         st.error("Please enter something before pressing Submit!")
