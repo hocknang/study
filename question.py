9051634b779c45b5a04044e4c97be0942b8375b5
hocknang <fang_peiyuan@yahoo.com>
2024-10-22 14:37:37 +0800

    TEST

--- question.py
+++ question.py
@@ -18,14 +18,22 @@ def display_mcq(question, options):
 def home():
     # Define your questions and options
     questions = {
-        "What is the capital of France?": ["Berlin", "Madrid", "Paris", "Lisbon"],
-        "What is 2 + 2?": ["3", "4", "5", "6"],
-        "Which planet is known as the Red Planet?": ["Earth", "Mars", "Jupiter", "Saturn"],
+        "Who is eligible to rent out their flat?": ["Singapore Permanent Residents", "Singapore Citizens", "Foreigners", "Both A and C"],
+        "What is the Minimum Occupation Period (MOP) for a non-subsidised flat purchased before 30 Aug 2010?": ["3 years", "5 years", "1 year", "No MOP required"],
+        "Which of the following is NOT allowed to rent the flat?": ["Singapore Citizens", "Non-Malaysian Work Permit holders from the Construction sector", ") Singapore Permanent Residents", "Students on a Student Pass"],
+	"How long must a tenant occupy the flat before they can be replaced by a new occupier?": ["3 months", "6 months", "12 months", "24 months"],
+	"What is the maximum period for renting out a flat per application?": ["12 months", "24 months", "36 months", "48 months"],
+	"If the flat owner is overseas during the rental period, what is required?": ["No additional action needed", "A certified copy of the Power of Attorney", "Notification to HDB", "All of the above"],
+	"What is the consequence of renting out the flat without HDB's approval?": ["The rental agreement is void", "HDB may take action to repossess the flat", "The flat owner receives a warning", "There are no consequences"],
+	"How many persons can live in a 3-room flat?": ["4", "6", "8", "10"],
+	"The flat owner can keep renting?": ["The flat owner can keep renting", "HDB's consent to rent is automatically revoked", "The flat owner must inform the tenant", "Nothing happens"],
+        "How much is the administrative fee for each rental application?": ["$10", "$18", "$50", "$100"],
     }
 
     # Initialize score
     score = 0
-    correct_answers = ["Paris", "4", "Mars"]  # List of correct answers
+    correct_answers = ["Singapore Citizens", "3 years", "Non-Malaysian Work Permit holders from the Construction sector", "6 months", "36 months", "A certified copy of the Power of Attorney",
+		       "HDB may take action to repossess the flat", "6", "HDB's consent to rent is automatically revoked", "$18"]  # List of correct answers
 
     # Display questions
     for question, options in questions.items():
