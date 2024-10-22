74a5f5fd5ce23c1dd4ddd0bee73741358f9319ea
hocknang <fang_peiyuan@yahoo.com>
2024-10-22 12:05:35 +0800

    test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import lesson as lesson
 import learn as learn
+import question as question
 
 #Define the pages: 
 def page_home():
@@ -8,11 +9,15 @@ def page_home():
 
 def page_learn():
     learn.home()
+
+def page_question():
+    question.home()
     
 # Create a dictionary of pages
 pages = {
     "Explorer Chatbots": page_home,
-    "Testing Chatbots" : page_learn
+    "Testing Chatbots" : page_learn,
+    "Knowledge & Understanding (MCQ)" : page_question 
 }
 
 # Add a sidebar for navigation
