189225fd9ce193cc43eb5b15a39cc93430097330
peiyuan <fang_peiyuan@yahoo.com>
2024-10-23 10:44:37 +0800

    test

--- question.py
+++ question.py
@@ -19,16 +19,16 @@ def display_mcq(question, options, key):
 def home():
     # Define your questions and options
     questions = {
-        "1. Who is eligible to rent out their flat?": ["Singapore Permanent Residents", "Singapore Citizens", "Foreigners", "Both A and C"],
-        "2. What is the Minimum Occupation Period (MOP) for a non-subsidised flat purchased before 30 Aug 2010?": ["3 years", "5 years", "1 year", "No MOP required"],
-        "3. Which of the following is NOT allowed to rent the flat?": ["Singapore Citizens", "Non-Malaysian Work Permit holders from the Construction sector", "Singapore Permanent Residents", "Students on a Student Pass"],
-	"4. How long must a tenant occupy the flat before they can be replaced by a new occupier?": ["3 months", "6 months", "12 months", "24 months"],
-	"5. What is the maximum period for renting out a flat per application?": ["12 months", "24 months", "36 months", "48 months"],
-	"6. If the flat owner is overseas during the rental period, what is required?": ["No additional action needed", "A certified copy of the Power of Attorney", "Notification to HDB", "All of the above"],
-	"7. What is the consequence of renting out the flat without HDB's approval?": ["The rental agreement is void", "HDB may take action to repossess the flat", "Notification to HDB", "The flat owner receives a warning"],
-	"8. How many persons can live in a 3-room flat?": ["4", "6", "8", "10"],
-	"9. What happens if a non-citizen tenant's permission to remain in Singapore is revoked": ["The flat owner can keep renting", "HDB's consent to rent is automatically revoked", "The flat owner must inform the tenant", "Nothing happens"],
-	"10. How much is the administrative fee for each rental application?": ["$10", "$18", "$50", "$100"],
+        "Who is eligible to rent out their flat?": ["Singapore Permanent Residents", "Singapore Citizens", "Foreigners", "Both A and C"],
+        "What is the Minimum Occupation Period (MOP) for a non-subsidised flat purchased before 30 Aug 2010?": ["3 years", "5 years", "1 year", "No MOP required"],
+        "Which of the following is NOT allowed to rent the flat?": ["Singapore Citizens", "Non-Malaysian Work Permit holders from the Construction sector", "Singapore Permanent Residents", "Students on a Student Pass"],
+	"How long must a tenant occupy the flat before they can be replaced by a new occupier?": ["3 months", "6 months", "12 months", "24 months"],
+	"What is the maximum period for renting out a flat per application?": ["12 months", "24 months", "36 months", "48 months"],
+	"If the flat owner is overseas during the rental period, what is required?": ["No additional action needed", "A certified copy of the Power of Attorney", "Notification to HDB", "All of the above"],
+	"What is the consequence of renting out the flat without HDB's approval?": ["The rental agreement is void", "HDB may take action to repossess the flat", "Notification to HDB", "The flat owner receives a warning"],
+	"How many persons can live in a 3-room flat?": ["4", "6", "8", "10"],
+	"What happens if a non-citizen tenant's permission to remain in Singapore is revoked": ["The flat owner can keep renting", "HDB's consent to rent is automatically revoked", "The flat owner must inform the tenant", "Nothing happens"],
+	"How much is the administrative fee for each rental application?": ["$10", "$18", "$50", "$100"],
     }
 
     # Define correct answers
@@ -55,7 +55,3 @@ def home():
         for i, question in enumerate(questions.keys()):
             st.write(f"**Question:** {question }")
             st.write(f"**Answer:** {answer[i]}")
-	    #st.write(f"Your answer: {answer[i]}. Correct answer: {correct_answer[i]}")
-            
-	    #st.write("Your answer: {answers[i]})
-	    #st.write(Correct answer: ")
