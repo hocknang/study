996ec776b587d6d7dfc00f45def8313c197532ee
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 11:25:00 +0800

    test

--- lesson.py
+++ lesson.py
@@ -3,12 +3,40 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from io import BytesIO
 from openai import OpenAI
-import chatbotMethod as chatbotMethod
 
 
 def chatbot(pdf_text):
     #working
-    chatbotMethod.home(pdf_text)
+    st.title("Chatbot Learning")
+
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o-mini"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
 
 def pdfReader(document_url, uploaded_file):
     if document_url is not None:
