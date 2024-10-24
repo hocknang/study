7a84eaf28a5ba8cf52db064627c834260a05b7ac
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 09:26:18 +0800

    test

--- learn.py
+++ learn.py
@@ -5,3 +5,7 @@ def home():
 
      with st.chat_message("user"):
           st.write("Hello 👋")
+
+     prompt = st.chat_input("Say something")
+     if prompt:
+          st.write(f"User has sent the following prompt: {prompt}")
