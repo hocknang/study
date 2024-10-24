1a42f90c495aff3afa5cf01ba9f32bb7c192b8a5
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 11:14:13 +0800

    test

--- chatbotMethod.py
+++ chatbotMethod.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 def home(pdf_text):
     #st.write(pdf_text)
 
+    # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
 
@@ -12,3 +13,17 @@ def home(pdf_text):
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
