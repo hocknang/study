28d659004181adf218f2a023ba259f4c30aa5689
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 10:15:50 +0800

    test

--- learn.py
+++ learn.py
@@ -4,13 +4,6 @@ from openai import OpenAI
 def home():
      st.title("Chatbot Learning")
 
-     with st.chat_message("user"):
-          st.write("Hello 👋")
-
-     prompt = st.chat_input("Say something")
-     if prompt:
-          st.write(f"User has sent the following prompt: {prompt}")
-
      client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
      if "openai_model" not in st.session_state:
