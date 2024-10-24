9b9363b82e60971a9cba8d17e7f5f3b74bee23ff
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 14:12:31 +0800

    test

--- lesson.py
+++ lesson.py
@@ -50,6 +50,10 @@ def readPDF(response):
         st.write("Not able to read the pdf")
 
 def home():
+    st.expander("IMPORTANT NOTICE: This web application is developed as a proof-of-concept prototype. The information provided here is NOT intended for actual usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters."
+                "Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output."
+                "Always consult with qualified professionals for accurate and personalized advice.")
+
     st.title("Term & Condition")
     st.write('Hi this is Term & Condition Explorer ChatBot')
 
@@ -140,8 +144,6 @@ def home():
             )
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
-
-
 
     '''
     if st.button("Submit"):
