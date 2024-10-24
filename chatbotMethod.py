d70de6babb498fceee66728be1599d1bbf3ac1d4
peiyuan <fang_peiyuan@yahoo.com>
2024-10-24 11:06:09 +0800

    test

--- chatbotMethod.py
+++ chatbotMethod.py
@@ -6,3 +6,18 @@ def home(pdf_text):
     #st.write(pdf_text)
 
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o-mini"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
