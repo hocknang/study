fbaa5dc1f098a6ea9dc45dcd6085c4aea05303f3
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 15:36:54 +0800

    new

--- method.py
+++ method.py
@@ -3,4 +3,6 @@ import streamlit as st
 def home():
     st.title("Methodology")
 
+    st.subheader("Retrieval Augmented Generation (RAG)")
+
     st.image("https://github.com/hocknang/study/blob/main/resource/RAG1_Diagram.png?raw=true")
