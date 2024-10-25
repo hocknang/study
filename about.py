ab01d1f40e5381dce606243efd315af97fef8c2f
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 12:04:20 +0800

    new

--- about.py
+++ about.py
@@ -16,8 +16,9 @@ def home():
 
     st.markdown('''
                 With Large Language Models :blue-background[(LLMs)] people can :blue-background[search] the 
-                Terms and Conditions blue-background[(T&C)] by :blue-background[typing questions] in their own words, without needing to know any special
-                legal terms. The LLM also helps by turning complicated legal words into simple language that anyone 
-                can understand.''')
+                Terms and Conditions blue-background[(T&C)] by :blue-background[typing questions] in their 
+                :blue-background[own words], without needing to know any special
+                legal terms. The LLM also helps by turning :blue-background [complicated legal words into simple language]
+                that anyone can understand.''')
 
 
