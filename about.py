dee7f69b3f40e491ace195051c7113d32f6bb6ec
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:09:04 +0800

    new

--- about.py
+++ about.py
@@ -6,9 +6,11 @@ def home():
 
     st.subheader("Problem Statement")
 
+    '''
     st.markdown(f"Uses finds it very **{inefficiency}** and time-consuming process of navigating through extensive "
              "Terms and Conditions (T&C) document. The current document is overly lengthy, making it difficult "
              "for users to quickly locate specific clauses or key information.")
+    '''
 
     st.markdown('''
             :black[Uses finds it very]''')
