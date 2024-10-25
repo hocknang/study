6fc699789312870916e6215688238d186f4e167a
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:04:13 +0800

    new

--- about.py
+++ about.py
@@ -5,4 +5,8 @@ def home():
 
     st.subheader("Problem Statement")
 
-    st.write()
+    st.write("Uses finds it very inefficiency and time-consuming process of navigating through extensive "
+             "Terms and Conditions (T&C) document. The current document is overly lengthy, making it difficult "
+             "for users to quickly locate specific clauses or key information.")
+
+    
