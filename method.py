e35ba52c40932733b3955173f9701ad2d2d67cf8
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 15:35:46 +0800

    new

--- method.py
+++ method.py
@@ -3,4 +3,4 @@ import streamlit as st
 def home():
     st.title("Methodology")
 
-    st.image("https://github.com/hocknang/study/blob/main/resource/RAG1_Diagram.png")
+    st.image("https://github.com/hocknang/study/blob/main/resource/RAG1_Diagram.png?raw=true")
