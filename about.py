e0fea88720c007336f94812eb827de92f4af2634
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:14:18 +0800

    new\

--- about.py
+++ about.py
@@ -14,7 +14,9 @@ def home():
 
     st.markdown('''
             Uses finds it very :red[inefficiency] and :red[time-consuming] process of navigating through extensive
-            :blue-background[Terms and Conditions (T&C) document]''')
+            :blue-background[Terms and Conditions (T&C) document]. 
+            The current document is overly red[lengthy], making it difficult "
+             "for users to quickly :blue-background[locate specific clauses or key information]''')
 
     st.markdown('''
         :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
