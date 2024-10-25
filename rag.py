4a935a6b42b2c353e46547813190e38541ce5ba3
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 16:05:04 +0800

    new

--- rag.py
+++ rag.py
@@ -81,7 +81,7 @@ def readPDF(response):
 def init():
     st.write("Please enter your password:")
 
-    password = "";
+    password = ""
 
     password = st.text_input("Password", type="password")
     if st.secrets["PASSWORD"] == password:
