e0a1c2bf59dffeb871f0adebd7d64bd316849bed
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 15:39:56 +0800

    new

--- method.py
+++ method.py
@@ -8,3 +8,6 @@ def home():
     st.image("https://github.com/hocknang/study/blob/main/resource/RAG1_Diagram.png?raw=true")
 
     st.subheader("Vector Store Loading")
+
+    st.markdown(''':blue-background[Step 1:] User can choose to :blue-background[Upload a file] 
+    or Provide a :blue-background[Document URL]''')
