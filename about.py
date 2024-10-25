b9de2502f1a40a4cc9c04182bf94bebfa71b8a0a
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:05:07 +0800

    new

--- about.py
+++ about.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
 def home():
+    inefficiency = "inefficiency"
+
     st.title("About Us")
 
     st.subheader("Problem Statement")
 
-    st.write("Uses finds it very inefficiency and time-consuming process of navigating through extensive "
+    st.write(f"Uses finds it very **{inefficiency}** and time-consuming process of navigating through extensive "
              "Terms and Conditions (T&C) document. The current document is overly lengthy, making it difficult "
              "for users to quickly locate specific clauses or key information.")
 
-    
+
