7df5840a7bdc1200fc8e9ef11cc9eda87ddf70b6
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 15:46:31 +0800

    new

--- method.py
+++ method.py
@@ -12,4 +12,5 @@ def home():
     st.markdown(''':blue-background[Step 1:] User can choose to :blue-background[Upload a file] 
     or Provide a :blue-background[Document URL]''')
 
-    st.markdown(''':blue-background[Step 2:] The text from the documents is split into smaller chunks or segments.''')
+    st.markdown(''':blue-background[Step 2:] The text from the :blue-background[documents]
+     is split into :blue-background[smaller chunks] or :blue-background[segments.]''')
