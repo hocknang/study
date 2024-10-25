79314e99abbe9ded660f83a9eaa85ef4daafddab
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 15:37:40 +0800

    new

--- method.py
+++ method.py
@@ -6,3 +6,5 @@ def home():
     st.subheader("Retrieval Augmented Generation (RAG)")
 
     st.image("https://github.com/hocknang/study/blob/main/resource/RAG1_Diagram.png?raw=true")
+
+    st.subheader("Vector Store Loading")
