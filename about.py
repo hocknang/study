128289b76683188e383488288c9dcb9f71f8b5e1
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:12:46 +0800

    new\

--- about.py
+++ about.py
@@ -13,7 +13,7 @@ def home():
     '''
 
     st.markdown('''
-            Uses finds it very :red[inefficiency] and :red[time-consuming] ''')
+            Uses finds it very :red[inefficiency] and :red[time-consuming] process of navigating through extensive''')
 
     st.markdown('''
         :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
