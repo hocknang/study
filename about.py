cd507b421c7d3bd28bc2128e684f25a0383ea1b9
peiyuan <fang_peiyuan@yahoo.com>
2024-10-25 11:57:21 +0800

    new\

--- about.py
+++ about.py
@@ -5,13 +5,14 @@ def home():
     st.title("About Us")
 
     st.subheader("Problem Statement")
-    
+
     st.markdown('''
             Uses finds it very :red[inefficiency] and :red[time-consuming] process of navigating through extensive
             :blue-background[Terms and Conditions (T&C) document]. 
             The current document is overly :red[lengthy], making it difficult 
             for users to quickly :blue-background[locate specific clauses or key information]''')
 
+    st.subheader("Proposed Solution")
 
 
 
